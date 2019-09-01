f9b3302d7a3e5755500dac0e5c1c79337d485bf4
ClementBouchet <bouchetclem@gmail.com>
2019-09-01 22:19:42 +0200

    refacto modifyAddress method

--- src/main/java/Subscriber.java
+++ src/main/java/Subscriber.java
@@ -1,24 +1,15 @@
-import java.util.List;
-
 public class Subscriber {
 
-    private Contract contract;
-    private List<Contract> contracts;
+    private ContractList contracts;
 
     public Subscriber(SubscriberId subscriberId, SubscriberAddress address) {
     }
 
-    public Subscriber(SubscriberId subscriberId, SubscriberAddress address, Contract contract) {
-        this.contract = contract;
-    }
-
-    public Subscriber(SubscriberId anyId, SubscriberAddress initialAddress, List<Contract> contracts) {
+    public Subscriber(SubscriberId anyId, SubscriberAddress initialAddress, ContractList contracts) {
         this.contracts = contracts;
     }
 
     public void modifyAddress(SubscriberAddress newAddress, EffectiveDate effectiveDate) {
-        for(Contract contract : contracts){
-            contract.setSubscriberAddress(newAddress);
-        }
+        contracts.applyAddressChangeOnAll(newAddress);
     }
 }

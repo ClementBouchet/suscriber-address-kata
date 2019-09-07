577dbb592486361ce5d6c319d50ee41d939fc442
ClementBouchet <bouchetclem@gmail.com>
2019-09-07 17:19:57 +0200

    delete unused fields

--- src/main/java/fr/lacombe/Model/SubscriberContract.java
+++ src/main/java/fr/lacombe/Model/SubscriberContract.java
@@ -3,14 +3,10 @@ package fr.lacombe.Model;
 import fr.lacombe.ContractId;
 
 public class SubscriberContract {
-    private SubscriberId subscriberId;
     private SubscriberAddress subscriberAddress;
-    private ContractId contractId;
 
     public SubscriberContract(SubscriberId subscriberId, SubscriberAddress subscriberAddress, ContractId contractId) {
-        this.subscriberId = subscriberId;
         this.subscriberAddress = subscriberAddress;
-        this.contractId = contractId;
     }
 
     public SubscriberAddress getAddress() {
